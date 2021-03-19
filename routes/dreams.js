b12cfe9e40c432d7e8aa3c19104f30c9325ca14a
nebojsaj1726 <nebojsa.jacovic@gmail.com>
2021-03-19 19:01:48 +0100

    CRUD endpoints

--- routes/dreams.js
+++ routes/dreams.js
@@ -7,4 +7,64 @@ router.get("/enum", (req, res) => {
   res.send(allDreamTypes);
 });
 
+router.post("/", async (req, res) => {
+  const dream = new Dream({
+    title: req.body.title,
+    description: req.body.description,
+    type: req.body.type,
+  });
+
+  try {
+    const savedDream = await dream.save();
+    res.json(savedDream);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const dreams = await Dream.find();
+    res.json(dreams);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.get("/:dreamId", async (req, res) => {
+  try {
+    const dream = await Dream.findById(req.params.dreamId);
+    res.json(dream);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.patch("/:dreamId", async (req, res) => {
+  try {
+    const updatedDream = await Dream.updateOne(
+      { _id: req.params.dreamId },
+      {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+          type: req.body.type,
+        },
+      }
+    );
+    res.json(updatedDream);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+router.delete("/:dreamId", async (req, res) => {
+  try {
+    const deletedDream = await Dream.deleteOne({ _id: req.params.dreamtId });
+    res.json(deletedDream);
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
 module.exports = router;
